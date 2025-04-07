6802ce92a3e3b7c3c7d9c02d4a1459922674c16d
Juho T. <juho.taskinen@edu.turkuamk.fi>
2025-04-07 10:29:53 +0300

    Fixed the encoder mapping

--- keyboards/muikku_keyboards/muikku58/keymaps/vial/keymap.c
+++ keyboards/muikku_keyboards/muikku58/keymaps/vial/keymap.c
@@ -67,17 +67,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),            },
-    [2] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),            },
-    [3] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),           },
-    [4] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), },
-    [5] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           },
-    [6] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),            },
-    [7] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),       },
-    //                  Encoder 1                                     Encoder 2
+    [0] =   { ENCODER_CCW_CW(KC_A, KC_B), ENCODER_CCW_CW(KC_D, KC_F)  },
+    [1] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [2] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [3] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [4] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [5] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [6] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [7] =   { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  }
 };
 #endif
